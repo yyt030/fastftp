0bf743c4f65a99eb2b6d0d203f6b9a8e355b0f5b
yueyt <yyt030@gmail.com>
2018-02-22 12:22:15 +0800

    update

--- client/client.go
+++ client/client.go
@@ -8,23 +8,24 @@ import (
 	"fmt"
 )
 
+var wg sync.WaitGroup
+
 func main() {
 	const chunkCount = 30
 	const fileSize = 865075200
 	const srcFilename = "testdata/ubuntu-16.04.3-server-amd64.iso"
 
-	var wg sync.WaitGroup
 	wg.Add(chunkCount)
 
 	chunkSize := int(math.Ceil(float64(fileSize) / float64(chunkCount)))
 	for i := 0; i < chunkCount; i++ {
-		go ReadWriteChunk(srcFilename, chunkSize, int64(i*chunkSize), &wg)
+		go ReadWriteChunk(srcFilename, chunkSize, int64(i*chunkSize))
 	}
 	fmt.Println(">>> goroutine waiting...")
 	wg.Wait()
 }
 
-func ReadWriteChunk(src string, chunkSize int, offset int64, wg *sync.WaitGroup) {
+func ReadWriteChunk(src string, chunkSize int, offset int64) {
 	chunk, newOffset := common.ReadChunkFromFile(src, chunkSize, offset)
 
 	conn, err := net.Dial("tcp", "172.16.66.132:8888")
